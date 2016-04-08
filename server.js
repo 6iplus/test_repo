a21e3214fe335b502bc96e0d117726b3cbaa215a
sd6473170 <sd6473170@gmail.com>
2016-04-08 15:36:19 -0400

    This is the server of Tianchi_Liu's

--- server.js
+++ server.js
@@ -1,92 +1,58 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mySongList = require('./songList.js');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var Guid = require('Guid');
+var partyData = require('./partydata.js');
 
 app.set('view engine','ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/assets', express.static('static'));
-var partyId = "party1";
 
-app.get("/party/:partyId", function (req,res) {
-    
-    var partyId = req.params.partyId;
-    //check if the partyId in partList
-    //if not show error
-    mySongList.findPartyByPartyID(partyId).then(function(party){
-        console.log(party);
-	    res.render('pages/party',{party: party});
-    }, function(error){
-        console.log(error);
-    });
+
+//index
+app.get("/", function (req,res) {
+	res.render("pages/index", {Inf: "Welcome!"});
 });
 
-//socket.io start
+//redirect to partyconfig
+app.get("/createparty", function(request, response){
+	if(!response.locals.user||response.locals.user==undefined){ 
+	response.render("pages/index", {Inf: "please log in first!"});
+}else{
+	response.render("pages/partyconfig", {Inf: "please input your party information"});
+}
 
-io.on('connection', function(socket){
-     socket.on('room', function(room) {
-         socket.join(room);
-         console.log("user joined room: " + room);
-              // now, it's easy to send a message to just the clients in a given room
-         socket.in(room).on('chat message', function(data){
-            console.log(room , data);
-            io.in(room).emit('chat message', data);
-        });
-     });
-  });
-
-//socket.io end
-
-
-
-app.get("/", function(request, respond){
-    
-    
-    
-    mySongList.getAllComments().then(function (commentList) {
-        response.render("pages/home", { error: null, comments: commentList });
-    }, function (errorMessage) {
-        response.status(500).json({ error: errorMessage });
-    });
-    respond.render("pages/songList");
 });
 
-app.post("/party/:partyId", function(request, respond) {
-    
+//create party
+app.post("/createparty", function(request, response){
+if(!response.locals.user||response.locals.user==undefined){ 
+	response.render("pages/index", {Inf: "please log in first!"});
+}else{
+	var partyId = request.body.partyId;
+	var partyName = request.body.partyName;
+	if(!response.locals.user||response.locals.user==undefined){ 
+	var createdBy = "unknown user";
+}
+else{
+	var createdBy = response.locals.user.username;
+}
+	var playList = {};
+	var config = {};
+	partyData.getPartyById(partyId).then(function(partyList){
+if(partyList.length>0){
+response.render("pages/partyconfig", {Inf: "party id existed"});
+}else{
+	partyData.createParty(partyId, partyName, createdBy, playList, config);
+	response.render("pages/party/:partyId", {partyId: partyId});
+}
 });
+}
+	
+	});
 
 
-app.get("/party/songList/:id", function (req,res) {
-	var id = req.params.id;
-	var result = mySongList.get(partyId, id);
-	res.json(result);
-});
-
-app.post("/party", function(req,res) {
-	var result = mySongList.createParty(req.body.partyId);
-    partyId = req.body.partyId;//todo remember to remove this later no way to write like this
-	res.json(result);
-});
-
-
-app.post("/party/songList", function(req,res) {
-	var  songName = req.body.songName,
-		 url = req.body.url,
-		 owner = 'Sunny';
-	var result = mySongList.create(partyId,songName,url,owner);
-    io.in(partyId).emit('chat message', result);
-	res.json(result);
-});
-
-app.delete("/party/songList/:id", function (req,res) {
-	var id = req.params.id;
-	var result = mySongList.delete(partyId, id);
-	res.json(result);
-});
-
-http.listen(3000, function () {
+app.listen(3000, function () {
 	console.log('Your server is now listening on port 3000! Navigate to http://localhost:3000 to access it');
 })
